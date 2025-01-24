3afd6b3d8e315052aaefa42b9b7f77df3aba24d5
Adria Congostrina <adria.congostrina.7e8@itb.cat>
2025-01-24 15:02:18 +0100

    update

---
+++ E02/main.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# Ruta de los programas
+ruta_programas = 'C:/Users/adria/Desktop/VM/TA06-Grup3-Izan-Miquel-Jordi/E02'
+
+# Comandos para ejecutar cada uno de los programas (ajustando los nombres según corresponda)
+comandos = [
+    ['python', f'{ruta_programas}/PAS1.py'],
+    ['python', f'{ruta_programas}/PAS2.py'],
+    ['python', f'{ruta_programas}/PAS3.py'],
+    ['python', f'{ruta_programas}/PAS4.py']
+]
+
+# Ejecutar los programas en secuencia
+for comando in comandos:
+    try:
+        subprocess.run(comando, check=True)
+        print(f'{comando[1]} ejecutado con éxito')
+    except subprocess.CalledProcessError as e:
+        print(f'Error al ejecutar {comando[1]}: {e}')
